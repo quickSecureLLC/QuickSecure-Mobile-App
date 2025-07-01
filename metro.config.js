1d94596aff32ec58ee5250465faaf3d15d4daae9
quickSecureLLC <quicksecurellc@gmail.com>
2025-07-01 15:29:50 -0400

    Fix metro.config.js: use getDefaultConfig only

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,3 @@
-const { getDefaultConfig, mergeConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
-const projectConfig = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: ['png', 'jpg', 'jpeg', 'gif', 'webp'],
-    sourceExts: ['js', 'jsx', 'ts', 'tsx', 'svg'],
-  },
-};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), projectConfig); 
+module.exports = getDefaultConfig(__dirname); 
